8110b0c3495d559e61867a4dc6d4322de58f6e32
Mahavidya <104791133+VidyaDesign@users.noreply.github.com>
2024-07-07 11:57:21 -0300

    Add files via upload

---
+++ IMC/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let peso = Number(prompt("Digite o seu peso"));
+let altura = Number(prompt("Digite sua altura"));
+let imc = peso / (altura * 2);
+console.log(`IMC: ${imc}`);
---
+++ IMC/src/index.ts
@@ -0,0 +1,4 @@
+let peso = Number(prompt("Digite o seu peso"));
+let altura = Number(prompt("Digite sua altura"));
+let imc = peso / (altura * 2);
+console.log(`IMC: ${imc}`);
